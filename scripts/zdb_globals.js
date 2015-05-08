895279751cc5ce4d5be2f21daddc674eb8703f3e
Carsten Klee <carsten.klee@sbb.spk-berlin.de>
2015-05-08 08:25:51 +0200

    new globals delimiter

---
+++ scripts/zdb_globals.js
@@ -0,0 +1,3 @@
+var delimiter = '\u0192'; // Unterfeldzeichen "ƒ" = \u0192
+var delimiterReg = '\u0192'; // regualr expression version Unterfeldzeichen "$" = \$
+var charCode = 402; // Unterfeldzeichen "ƒ" = 402, Unterfeldzeichen "$" = 36
